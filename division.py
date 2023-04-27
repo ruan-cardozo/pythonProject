6424c303c2a328941bff2177707f993e2abfffb7
ruanvcardozo@gmail.com <110867639+ruan-cardozo@users.noreply.github.com>
2023-04-27 20:01:51 -0300

    add division.py

--- division.py
+++ division.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-app = Flask(__name__)
-api = Api(app)
-
 class Multiply(Resource):
     def get(self):
         num1 = int(request.args.get('num1'))
