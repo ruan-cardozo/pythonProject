e69444ce87023662b41ddbc968152c163001d6d4
ruanvcardozo@gmail.com <110867639+ruan-cardozo@users.noreply.github.com>
2023-04-27 20:12:51 -0300

    add e subtract

--- app.py
+++ app.py
@@ -5,6 +5,22 @@ app = Flask(__name__)
 api = Api(app)
 
 
+class Add(Resource):
+    def get(self):
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        result = num1 + num2
+        return jsonify({'result': result})
+
+
+class Subtract(Resource):
+    def get(self):
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        result = num1 - num2
+        return jsonify({'result': result})
+
+
 class Multiply(Resource):
     def get(self):
         num1 = int(request.args.get('num1'))
@@ -18,8 +34,12 @@ class Division(Resource):
     def get(self):
         num1 = int(request.args.get('num1'))
         num2 = int(request.args.get('num2'))
+        result = num1 / num2
+        return jsonify({'result': result})
 
 
+api.add_resource(Add, '/Add')
+api.add_resource(Subtract, '/Subtract')
 api.add_resource(Multiply, '/multiply')
 api.add_resource(Division, '/division')
 
